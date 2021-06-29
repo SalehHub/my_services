1b1811209ca660b468183aa2429357d95d80d694
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-06-29 07:33:35 +0300

    Update my_text_input.dart

--- lib/widgets/my_text_input.dart
+++ lib/widgets/my_text_input.dart
@@ -7,19 +7,20 @@ class MyTextInput extends StatelessWidget {
     Key? key,
     this.value,
     this.validator,
-    this.isPassword = false,
     this.labelText,
     this.helperText,
     this.suffixIcon,
     this.prefixIcon,
     this.onChanged,
     this.textDirection,
-    this.textInputAction = TextInputAction.done,
     this.keyboardType,
-    this.radius = 10,
     this.controller,
     this.focusNode,
     this.onFieldSubmitted,
+    this.isPassword = false,
+    this.radius = 10,
+    this.textInputAction = TextInputAction.done,
+    this.margin = EdgeInsets.zero,
   }) : super(key: key);
 
   final String? value;
@@ -37,29 +38,34 @@ class MyTextInput extends StatelessWidget {
   final TextEditingController? controller;
   final FocusNode? focusNode;
   final Function(String value)? onFieldSubmitted;
+  final EdgeInsetsGeometry margin;
+
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller ?? (value != null ? TextEditingController(text: value) : null),
-      enableSuggestions: !isPassword,
-      validator: validator,
-      textDirection: textDirection,
-      obscureText: isPassword,
-      textInputAction: textInputAction,
-      keyboardType: keyboardType,
-      style: getTextTheme(context).bodyText1?.copyWith(height: 2.5),
-      decoration: InputDecoration(
-        labelStyle: getTextTheme(context).bodyText2,
-        labelText: labelText,
-        helperText: helperText,
-        contentPadding: const EdgeInsets.fromLTRB(12, 12, 12, 8),
-        prefixIcon: prefixIcon,
-        suffixIcon: suffixIcon,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(radius)),
+    return Padding(
+      padding: margin,
+      child: TextFormField(
+        controller: controller ?? (value != null ? TextEditingController(text: value) : null),
+        enableSuggestions: !isPassword,
+        validator: validator,
+        textDirection: textDirection,
+        obscureText: isPassword,
+        textInputAction: textInputAction,
+        keyboardType: keyboardType,
+        style: getTextTheme(context).bodyText1?.copyWith(height: 2.5),
+        decoration: InputDecoration(
+          labelStyle: getTextTheme(context).bodyText2,
+          labelText: labelText,
+          helperText: helperText,
+          contentPadding: const EdgeInsets.fromLTRB(12, 12, 12, 8),
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(radius)),
+        ),
+        onChanged: onChanged,
+        focusNode: focusNode,
+        onFieldSubmitted: onFieldSubmitted,
       ),
-      onChanged: onChanged,
-      focusNode: focusNode,
-      onFieldSubmitted: onFieldSubmitted,
     );
   }
 }
