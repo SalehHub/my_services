b990bc3936fcd2e20fef692c5721d4a1dbf331e0
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-07-16 09:45:24 +0300

    Update service_theme.dart

--- lib/services/service_theme.dart
+++ lib/services/service_theme.dart
@@ -137,4 +137,9 @@ bool isDark(BuildContext context) => getTheme(context).brightness == Brightness.
 
 bool isLight(BuildContext context) => getTheme(context).brightness == Brightness.light;
 
-Color getTextColor(BuildContext context, [Color colorWhenDark = Colors.white, Color colorWhenLight = Colors.black]) => isDark(context) ? colorWhenDark : colorWhenLight;
+Color getTextColor(
+  BuildContext context, {
+  Color colorWhenDark = Colors.white,
+  Color colorWhenLight = Colors.black,
+}) =>
+    isDark(context) ? colorWhenDark : colorWhenLight;
