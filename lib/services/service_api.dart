31a351ffb4107f171a6ae9874e8ad0692f7678dd
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-10-21 20:31:20 -0500

    add lang to api request

--- lib/services/service_api.dart
+++ lib/services/service_api.dart
@@ -4,14 +4,17 @@ class ServiceApi {
   static final Dio dio = Dio();
   static String domain = '';
 
-  static Options _dioOptions(String? accessToken) {
-    Map<String, dynamic> headers = <String, dynamic>{};
-    if (accessToken != null) {
-      headers = <String, dynamic>{
+  static Options _dioOptions(String? accessToken, String? lang) {
+    Map<String, dynamic> headers = <String, dynamic>{
+      if (accessToken != null) ...{
         'X-Requested-With': 'XMLHttpRequest',
         'Authorization': 'Bearer $accessToken',
-      };
-    }
+      },
+      if (lang != null) ...{
+        'X-Localization': lang,
+      },
+    };
+
     return Options(
       method: 'POST',
       headers: headers,
@@ -22,12 +25,13 @@ class ServiceApi {
   static Future<Response<Map<String, dynamic>>> postRequest(String url, Map<String, dynamic> formData, CancelToken? cancelToken) async {
     logger.d(formData);
     final String? accessToken = formData['access_token'] as String?;
+    final String? lang = formData['lang'] as String?;
     logger.i('$domain$url');
     final Response<Map<String, dynamic>> data = await dio.postUri<Map<String, dynamic>>(
       Uri.parse('$domain$url'),
       data: formData,
       cancelToken: cancelToken,
-      options: _dioOptions(accessToken),
+      options: _dioOptions(accessToken, lang),
     );
     return data;
   }
