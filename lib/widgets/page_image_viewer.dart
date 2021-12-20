8abe29f6dd6e327d7fd0553c52c8bc1955825a29
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-12-19 20:55:31 -0600

    Add image viewer Dot Indicators

--- lib/widgets/page_image_viewer.dart
+++ lib/widgets/page_image_viewer.dart
@@ -1,11 +1,19 @@
 import '../my_services.dart';
 
 class PageImageViewer extends ConsumerStatefulWidget {
-  const PageImageViewer({Key? key, this.title = '', required this.image, this.imageList = const [], this.popupMenuButton}) : super(key: key);
+  const PageImageViewer({
+    Key? key,
+    this.title = '',
+    required this.image,
+    this.imageList = const [],
+    this.popupMenuButton,
+    this.showDotIndicators = true,
+  }) : super(key: key);
   final String title;
   final List<String> imageList;
   final String image;
   final MyPopupMenu? popupMenuButton;
+  final bool showDotIndicators;
 
   @override
   _PageImageViewerState createState() => _PageImageViewerState();
@@ -13,6 +21,9 @@ class PageImageViewer extends ConsumerStatefulWidget {
 
 class _PageImageViewerState extends MainStateTemplate<PageImageViewer> {
   MyServicesLocalizationsData get labels => getMyServicesLabels(context);
+
+  @override
+  bool get showRefreshIndicator => false;
 
   @override
   bool get hideTopBanner => true;
@@ -24,6 +35,8 @@ class _PageImageViewerState extends MainStateTemplate<PageImageViewer> {
   String get title => widget.title;
 
   List<String> imageList = [];
+
+  String get selectedImage => imageList[index];
 
   int index = 0;
   PageController pageController = PageController(initialPage: 0);
@@ -56,15 +69,51 @@ class _PageImageViewerState extends MainStateTemplate<PageImageViewer> {
 
   @override
   List<Widget> get bodyChildren => [
+        SliverToBoxAdapter(child: buildIndicators()),
+        //
         SliverToBoxAdapter(
           child: SizedBox(height: pageHeight - 100, child: buildImages()),
-        )
+        ),
+        //
       ];
+
+  Widget buildIndicators() {
+    if (widget.showDotIndicators == false || imageList.length < 2) {
+      return const SizedBox();
+    }
+
+    return Container(
+      height: 20,
+      margin: const EdgeInsets.only(left: 50, right: 50, top: 0, bottom: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      decoration: BoxDecoration(
+        boxShadow: [BoxShadow(offset: const Offset(0, 1), color: Colors.black.withOpacity(0.2))],
+        color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          children: imageList.map((e) {
+            Color? color = Colors.white;
+            double size = 8;
+            if (e == selectedImage) {
+              color = null;
+              size = 10;
+            }
+            return Icon(Mdi.circle, color: color, size: size);
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
   Widget buildImages() {
     return PhotoViewGallery.builder(
       itemCount: imageList.length,
       pageController: pageController,
+      onPageChanged: (i) => setState(() => index = i),
       builder: (context, index) {
         return PhotoViewGalleryPageOptions(
           imageProvider: CachedNetworkImageProvider(imageList[index]),
