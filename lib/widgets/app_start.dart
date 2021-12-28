100bf46eeed268e692696252ba970576053383f1
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-12-27 22:50:52 -0600

    Update app_start.dart

--- lib/widgets/app_start.dart
+++ lib/widgets/app_start.dart
@@ -99,12 +99,7 @@ class AppLauncher {
 }
 
 class AppStart extends StatelessWidget {
-  const AppStart({
-    Key? key,
-    required this.delegates,
-    required this.title,
-    required this.homePage,
-  }) : super(key: key);
+  const AppStart({Key? key, required this.delegates, required this.title, required this.homePage}) : super(key: key);
 
   final List<LocalizationsDelegate<dynamic>> delegates;
   final String title;
@@ -113,18 +108,15 @@ class AppStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
-      final ThemeMode? themeMode = MyServices.watchThemeMode(ref);
-      final Locale? locale = MyServices.watchLocale(ref);
-
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: title,
         //
         localizationsDelegates: ServiceLocale.localizationsDelegates(delegates),
         supportedLocales: ServiceLocale.supportedLocales,
-        locale: locale,
+        locale: MyServices.watchLocale(ref),
         //
-        themeMode: themeMode,
+        themeMode: MyServices.watchThemeMode(ref),
         theme: ServiceTheme.lightTheme(),
         darkTheme: ServiceTheme.darkTheme(),
         //
@@ -138,63 +130,3 @@ class AppStart extends StatelessWidget {
     });
   }
 }
-
-// bool appWithFirebase = true;
-// bool appWithCrashlytics = true;
-// bool appInitGeneralState = true;
-// bool appWithFCM = true;
-//
-// Future<void> myServicesRunApp({
-//   required Locale defaultLocale,
-//   required List<Locale> supportedLocales,
-//   required String title,
-//   required Widget homePage,
-//   bool withFirebase = true,
-//   bool withCrashlytics = true,
-//   bool withFCM = true,
-//   bool initGeneralState = true,
-//   Color? lightAccentColor,
-//   Color? darkAccentColor,
-//   Overrides? overrides,
-//   List<LocalizationsDelegate<dynamic>> delegates = const [],
-// }) async {
-//   appWithFirebase = withFirebase;
-//   appWithCrashlytics = withCrashlytics;
-//   appWithFCM = withFCM;
-//   appInitGeneralState = initGeneralState;
-//
-//   WidgetsFlutterBinding.ensureInitialized();
-//
-//   ServiceTheme.lightAccentColor = lightAccentColor;
-//   ServiceTheme.darkAccentColor = darkAccentColor;
-//
-//   ServiceLocale.defaultLocale = defaultLocale;
-//   ServiceLocale.supportedLocales = supportedLocales;
-//   GeneralState generalState = GeneralState();
-//
-//   if (withFirebase) {
-//     await Firebase.initializeApp();
-//   }
-//
-//   if (withFirebase && withCrashlytics) {
-//     await ServiceFirebaseCrashlytics.register();
-//   }
-//
-//   if (initGeneralState) {
-//     generalState = await getGeneralState();
-//   }
-//
-//   runApp(
-//     ProviderScope(
-//       overrides: [
-//         initialGeneralStateProvider.overrideWithValue(generalState),
-//         if (overrides != null) ...await overrides(),
-//       ],
-//       child: AppStart(
-//         title: title,
-//         homePage: homePage,
-//         delegates: delegates,
-//       ),
-//     ),
-//   );
-// }
