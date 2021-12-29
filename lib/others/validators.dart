7e20d2700e523f9fb86e4957017a881798d385cb
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-12-28 22:06:33 -0600

    add defaultEmailValidator

--- lib/others/validators.dart
+++ lib/others/validators.dart
@@ -2,10 +2,21 @@ import '../my_services.dart';
 
 // ignore: prefer_function_declarations_over_variables
 FormFieldValidator<String> fieldIsRequiredValidator = (String? value) {
-  MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
-
   if (value == null || value.trim().isEmpty) {
+    MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
     return myServicesLabels.thisFieldIsRequired;
+  }
+  return null;
+};
+
+// ignore: prefer_function_declarations_over_variables
+FormFieldValidator<String> defaultEmailValidator = (String? value) {
+  value = value?.trim();
+  final RegExp emailRxp = RegExp(r'^[^\s@]+@[^\s@]+\.[^\s@]+$');
+
+  if (value == null || value.isEmpty || !emailRxp.hasMatch(value)) {
+    MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
+    return myServicesLabels.incorrectEmail;
   }
   return null;
 };
