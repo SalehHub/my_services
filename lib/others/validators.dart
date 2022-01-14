8a8c1669c93ce39a6d832e4c52054aed82c81a16
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-01-14 07:32:30 -0600

    Update validators.dart

--- lib/others/validators.dart
+++ lib/others/validators.dart
@@ -1,7 +1,7 @@
 import '../my_services.dart';
 
 // ignore: prefer_function_declarations_over_variables
-FormFieldValidator<String> fieldIsRequiredValidator = (String? value) {
+FormFieldValidator<String> defaultFieldRequiredValidator = (String? value) {
   if (value == null || value.trim().isEmpty) {
     MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
     return myServicesLabels.thisFieldIsRequired;
@@ -20,3 +20,36 @@ FormFieldValidator<String> defaultEmailValidator = (String? value) {
   }
   return null;
 };
+
+FormFieldValidator<String> defaultPasswordValidator({int length = 6}) => (String? value) {
+      MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
+
+      if (value == null || value.isEmpty) {
+        return myServicesLabels.enterAPassword;
+      } else if (value.length < length) {
+        return myServicesLabels.passwordLengthMustBeAtLeastCharacters;
+      }
+      return null;
+    };
+
+FormFieldValidator<String> defaultNewPasswordValidator({int length = 6}) => (String? value) {
+      MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
+
+      if (value == null || value.isEmpty) {
+        return myServicesLabels.enterANewPassword;
+      } else if (value.length < length) {
+        return myServicesLabels.passwordLengthMustBeAtLeastCharacters;
+      }
+      return null;
+    };
+
+FormFieldValidator<String> defaultCurrentPasswordValidator({int length = 6}) => (String? value) {
+      MyServicesLocalizationsData myServicesLabels = getMyServicesLabels(ServiceNav.navigatorKey.currentContext!);
+
+      if (value == null || value.isEmpty) {
+        return myServicesLabels.enterTheCurrentPassword;
+      } else if (value.length < length) {
+        return myServicesLabels.passwordLengthMustBeAtLeastCharacters;
+      }
+      return null;
+    };
