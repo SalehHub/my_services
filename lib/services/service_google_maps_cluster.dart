b55c661cb6d62b072b4f2c54af728710792ca802
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-01-20 09:07:50 -0600

    Update service_google_maps_cluster.dart

--- lib/services/service_google_maps_cluster.dart
+++ lib/services/service_google_maps_cluster.dart
@@ -111,4 +111,16 @@ class ServiceGoogleMapsCluster<T extends ClusterItem> {
 
     return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
   }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    final ByteData data = await rootBundle.load(path);
+    final ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    final ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+  }
+
+  Future<BitmapDescriptor> getBitmapDescriptorFromAssetBytes(String path, int width) async {
+    final Uint8List imageData = await getBytesFromAsset(path, width);
+    return BitmapDescriptor.fromBytes(imageData);
+  }
 }
