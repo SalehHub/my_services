9250addcb90be84864f9002d1a53c60c6adffaaf
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-01-28 10:07:56 -0600

    Update service_app_badger.dart

--- lib/services/service_app_badger.dart
+++ lib/services/service_app_badger.dart
@@ -3,8 +3,10 @@ import '../my_services.dart';
 class ServiceAppBadger {
   static Future<void> updateBadgeCount(int count) async {
     try {
-      if (await FlutterAppBadger.isAppBadgeSupported()) {
-        FlutterAppBadger.updateBadgeCount(count);
+      if (!kIsWeb) {
+        if (await FlutterAppBadger.isAppBadgeSupported()) {
+          FlutterAppBadger.updateBadgeCount(count);
+        }
       }
     } on PlatformException catch (e, s) {
       logger.e(e, e, s);
@@ -13,5 +15,9 @@ class ServiceAppBadger {
     }
   }
 
-  static void removeBadge() => FlutterAppBadger.removeBadge();
+  static void removeBadge() {
+    if (!kIsWeb) {
+      FlutterAppBadger.removeBadge();
+    }
+  }
 }
