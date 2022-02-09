59c3c060e403e1e392b548d5ad416df8a9a4f881
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-02-09 06:50:02 -0600

    Update my_loading_image.dart

--- lib/widgets/my_loading_image.dart
+++ lib/widgets/my_loading_image.dart
@@ -41,59 +41,10 @@ class MyLoadingImage extends StatelessWidget {
   // final PlaceholderWidgetBuilder? placeholderBuilder;
   // final LoadingErrorWidgetBuilder? errorBuilder;
 
-  ImageProvider get cachedImage => CachedNetworkImageProvider(url);
-
-  ImageProvider get retryImage => NetworkImageWithRetry(url, fetchStrategy: fetchStrategy ?? defaultFetchStrategy);
-
-  ImageProvider get image => useCacheImage ? cachedImage : retryImage;
-
-  Future<FetchInstructions> Function(Uri uri, FetchFailure? failure) get defaultFetchStrategy => (Uri uri, FetchFailure? failure) async {
-        if (!url.startsWith('http') || (failure != null && failure.httpStatusCode == 404)) {
-          return FetchInstructions.giveUp(uri: uri);
-        } else {
-          return FetchInstructions.attempt(uri: uri, timeout: const Duration(seconds: 5));
-        }
-      };
-
-  BorderRadius get circleBorderRadius => BorderRadius.circular(height);
-
-  BorderRadius get _borderRadius => borderRadius ?? BorderRadius.circular(radius);
-
-  Widget get placeHolder => buildImageContainer(
-        Image(
-          width: width,
-          height: height,
-          image: BlurHashImage(blurHash ?? 'LUMiyF-=_N?a#PD*IUslj^RPt6RP'),
-          fit: fit,
-        ),
-      );
-
-  OctoPlaceholderBuilder get defaultPlaceholderBuilder => (BuildContext context) => placeHolder;
-
-  OctoErrorBuilder get defaultErrorBuilder => (BuildContext context, Object error, StackTrace? stackTrace) => placeHolder;
-
   // PlaceholderWidgetBuilder get defaultPlaceholderBuilder => (BuildContext context, _) => placeHolder;
   // LoadingErrorWidgetBuilder get defaultErrorBuilder => (BuildContext context, _, __) => placeHolder;
 
-  Widget buildImageContainer(Widget child) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        color: borderColor,
-        borderRadius: circle == true ? circleBorderRadius : _borderRadius,
-        border: Border.all(color: borderColor ?? Colors.transparent, width: borderWidth),
-      ),
-      child: MyInk(
-        onTap: onTap,
-        borderRadius: circle == true ? circleBorderRadius : _borderRadius,
-        child: ClipRRect(
-          borderRadius: circle == true ? circleBorderRadius : (borderRadius ?? BorderRadius.circular(radius - borderWidth)),
-          child: child,
-        ),
-      ),
-    );
-  }
+  // Widget buildImageContainer(Widget child) {}
 
   @override
   Widget build(BuildContext context) {
@@ -129,15 +80,103 @@ class MyLoadingImage extends StatelessWidget {
         fit: fit,
       );
     }
+
+    late ImageProvider image;
+    if (useCacheImage) {
+      image = CachedNetworkImageProvider(url);
+    } else {
+      image = NetworkImageWithRetry(
+        url,
+        fetchStrategy: fetchStrategy ??
+            (Uri uri, FetchFailure? failure) async {
+              if (!url.startsWith('http') || (failure != null && failure.httpStatusCode == 404)) {
+                return FetchInstructions.giveUp(uri: uri);
+              } else {
+                return FetchInstructions.attempt(uri: uri, timeout: const Duration(seconds: 5));
+              }
+            },
+      );
+    }
+
+    final Widget placeHolder = ImageContainer(
+      child: Image(width: width, height: height, image: BlurHashImage(blurHash ?? 'LUMiyF-=_N?a#PD*IUslj^RPt6RP'), fit: fit),
+      width: width,
+      height: height,
+      borderRadius: borderRadius ?? BorderRadius.circular(radius),
+      onTap: onTap,
+      borderColor: borderColor,
+      borderWidth: borderWidth,
+      circle: circle,
+    );
+
+    // precacheImage(image,context);
+
+    // return buildImageContainer(Image(image: image, fit: fit, width: width, height: height));
+
     return OctoImage(
       alignment: alignment,
       width: width,
       height: height,
       fit: fit,
-      imageBuilder: (BuildContext context, Widget child) => buildImageContainer(child),
+      imageBuilder: (BuildContext context, Widget child) => ImageContainer(
+        child: child,
+        width: width,
+        height: height,
+        borderRadius: borderRadius ?? BorderRadius.circular(radius),
+        onTap: onTap,
+        borderColor: borderColor,
+        borderWidth: borderWidth,
+        circle: circle,
+      ),
       image: image,
-      placeholderBuilder: placeholderBuilder ?? defaultPlaceholderBuilder,
-      errorBuilder: errorBuilder ?? defaultErrorBuilder,
+      placeholderBuilder: placeholderBuilder ?? (BuildContext context) => placeHolder,
+      errorBuilder: errorBuilder ?? (BuildContext context, Object error, StackTrace? stackTrace) => Stack(children: [placeHolder, const Center(child: Icon(Icons.error))]),
+    );
+  }
+}
+
+class ImageContainer extends StatelessWidget {
+  const ImageContainer({
+    Key? key,
+    required this.child,
+    required this.width,
+    required this.height,
+    required this.borderRadius,
+    required this.onTap,
+    required this.borderColor,
+    required this.borderWidth,
+    required this.circle,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final Color? borderColor;
+  final double borderWidth;
+  final BorderRadius? borderRadius;
+  final bool circle;
+  final GestureTapCallback? onTap;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final BorderRadius circleBorderRadius = BorderRadius.circular(height);
+
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: borderColor,
+        borderRadius: circle == true ? circleBorderRadius : borderRadius,
+        border: Border.all(color: borderColor ?? Colors.transparent, width: borderWidth),
+      ),
+      child: MyInk(
+        onTap: onTap,
+        borderRadius: circle == true ? circleBorderRadius : borderRadius,
+        child: ClipRRect(
+          borderRadius: circle == true ? circleBorderRadius : borderRadius,
+          child: child,
+        ),
+      ),
     );
   }
 }
