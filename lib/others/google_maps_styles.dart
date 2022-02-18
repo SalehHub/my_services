ac8c888018a517e3a3979471862c4fcfdb71c3b9
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-02-17 18:25:12 -0600

    Update google_maps_styles.dart

--- lib/others/google_maps_styles.dart
+++ lib/others/google_maps_styles.dart
@@ -1,24 +1,24 @@
 //googleMaps
 import '../my_services.dart';
 
-var lightBgColor = "#" + (ServiceTheme.lightBgColor?.value.toRadixString(16) ?? "ffffff");
-var lightRoadColor = "#" + (ServiceTheme.lightAccentColor?.value.toRadixString(16) ?? "193a55");
+// var lightBgColor = "#" + (ServiceTheme.lightBgColor.value.toRadixString(16) );
+// var lightRoadColor = "#" + (ServiceTheme.lightAccentColor.value.toRadixString(16));
 
-String lightMapStyle() => mapStyleLight(lightBgColor, lightRoadColor);
+// String lightMapStyle() => mapStyleLight(lightBgColor, lightRoadColor);
 
-var darkBgColor = "#" + (ServiceTheme.darkBgColor?.value.toRadixString(16) ?? "161C1E");
-var darkRoadColor = "#" + (ServiceTheme.darkAccentColor?.value.toRadixString(16) ?? "193a55");
+// var darkBgColor = "#" + (ServiceTheme.darkBgColor.value.toRadixString(16) );
+// var darkRoadColor = "#" + (ServiceTheme.darkAccentColor.value.toRadixString(16) );
 //
-String darkMapStyle() => mapStyleDark(darkBgColor, darkRoadColor);
+// String darkMapStyle() => mapStyleDark(darkBgColor, darkRoadColor);
 //
-String darkMapStyleStatic() {
-  String darkBgColor = ("0x" + (ServiceTheme.darkBgColor?.value.toRadixString(16) ?? "161C1E")).replaceAll("0xff", "");
-  String darkRoadColor = ("0x" + (ServiceTheme.darkAccentColor?.value.toRadixString(16) ?? "193a55")).replaceAll("0xff", "");
+String darkMapStyleStatic(darkBgColor,darkRoadColor) {
+  // String darkBgColor = ("0x" + (ServiceTheme.darkBgColor.value.toRadixString(16))).replaceAll("0xff", "");
+  // String darkRoadColor = ("0x" + (ServiceTheme.darkAccentColor.value.toRadixString(16))).replaceAll("0xff", "");
   return "style=element:geometry%7Ccolor:0x212121&style=element:geometry.fill%7Ccolor:0x$darkBgColor&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x757575&style=element:labels.text.stroke%7Ccolor:0x212121&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway%7Celement:geometry.fill%7Ccolor:0x$darkRoadColor&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0x$darkRoadColor&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:geometry.fill%7Ccolor:0x033954&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d&size=480x360";
 }
 
-String lightMapStyleStatic() {
-  String lightRoadColor = ("0x" + (ServiceTheme.lightAccentColor?.value.toRadixString(16) ?? "161C1E")).replaceAll("0xff", "");
+String lightMapStyleStatic(lightRoadColor) {
+  // String lightRoadColor = ("0x" + ServiceTheme.lightAccentColor.value.toRadixString(16)).replaceAll("0xff", "");
   return "style=feature:road.highway%7Celement:geometry.fill%7Ccolor:0x$lightRoadColor&size=480x360";
 }
 
@@ -254,7 +254,7 @@ String mapStyleDark(bgColor, roadColor) => '''
 
 //TODO: refactor this
 String getMapImageFromLatLong(double lat, double lng, String googleMapKey, BuildContext context) {
-  String style = isDark(context) ? darkMapStyleStatic() : lightMapStyleStatic();
+  String style = isDark(context) ? darkMapStyleStatic(Theme.of(context).colorScheme.background,Theme.of(context).colorScheme.primary) : lightMapStyleStatic(Theme.of(context).colorScheme.primary);
   String lang = ServiceLocale.currentLocaleLangCode(context);
 
   String language = 'language=$lang';
