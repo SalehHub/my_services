3385ec523a132af75c6fda7838a7a4e28accc53c
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-02-27 14:06:44 -0600

    Update my_text.dart

--- lib/widgets/my_text.dart
+++ lib/widgets/my_text.dart
@@ -27,18 +27,18 @@ class MyText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle? _textStyle = style ?? getTextTheme(context).bodyText2;
+    TextStyle _textStyle = style ?? DefaultTextStyle.of(context).style;
     if (bold) {
-      _textStyle = _textStyle?.copyWith(fontWeight: FontWeight.bold);
+      _textStyle = _textStyle.copyWith(fontWeight: FontWeight.bold);
     }
     if (color != null) {
-      _textStyle = _textStyle?.copyWith(color: color);
+      _textStyle = _textStyle.copyWith(color: color);
     }
     if (shadows != null) {
-      _textStyle = _textStyle?.copyWith(shadows: shadows);
+      _textStyle = _textStyle.copyWith(shadows: shadows);
     }
     if (fontSize != null) {
-      _textStyle = _textStyle?.copyWith(fontSize: fontSize);
+      _textStyle = _textStyle.copyWith(fontSize: fontSize);
     }
 
     return Padding(
@@ -54,3 +54,57 @@ class MyText extends StatelessWidget {
     );
   }
 }
+
+// class MyText2 extends Text {
+//   MyText2(
+
+//     String? text, {
+//     Key? key,
+//     this.margin = EdgeInsets.zero,
+//     TextStyle? style,
+//     bool? bold = false,
+//     TextAlign? textAlign,
+//     int? maxLines,
+//     double? fontSize,
+//     Color? color,
+//     List<Shadow>? shadows,
+//     TextOverflow? overflow,
+
+//   }) : super(
+//           text ?? "",
+//           key: key,
+//           maxLines: maxLines,
+//           overflow: overflow,
+//           textAlign: textAlign,
+//           style: getStyle(style, bold, color, shadows, fontSize),
+//         );
+
+//   final EdgeInsetsGeometry? margin;
+
+//   static TextStyle? getStyle(style, bold, color, shadows, fontSize) {
+//     TextStyle? _textStyle = style ?? const TextStyle();
+//     if (bold) {
+//       _textStyle = _textStyle?.copyWith(fontWeight: FontWeight.bold);
+//     }
+//     if (color != null) {
+//       _textStyle = _textStyle?.copyWith(color: color);
+//     }
+//     if (shadows != null) {
+//       _textStyle = _textStyle?.copyWith(shadows: shadows);
+//     }
+//     if (fontSize != null) {
+//       _textStyle = _textStyle?.copyWith(fontSize: fontSize);
+//     }
+
+//     return _textStyle;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (margin != null) {
+//       return Padding(padding: margin!, child: super.build(context));
+//     }
+//     return super.build(context);
+//   }
+
+// }
