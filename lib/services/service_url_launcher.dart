b829a765b2b71020a9212cc16f21630b478f7f79
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-05-18 21:59:12 +0300

    Update service_url_launcher.dart

--- lib/services/service_url_launcher.dart
+++ lib/services/service_url_launcher.dart
@@ -9,14 +9,29 @@ class ServiceURLLauncher {
   Future<bool> launchUniversalLinkIos(String url) async {
     try {
       assert(url.trim() != '');
+
       logger.i(url);
-      Uri uri = Uri.dataFromString(url);
-      if (await canLaunchUrl(uri)) {
-        final bool try2 = await launchUrl(uri, mode: LaunchMode.platformDefault);
+
+      Uri uri = Uri.parse(url);
+      // print(uri.scheme);
+
+      bool result = await canLaunchUrl(uri);
+      // print("canLaunchUrl: $result");
+
+      if (result) {
+        final bool try2 = await launchUrl(uri, mode: LaunchMode.externalApplication);
+        // print("externalApplication: $try2");
+
         if (try2 == false) {
-          final bool try3 = await launchUrl(uri, mode: LaunchMode.externalApplication);
+          final bool try3 = await launchUrl(uri, mode: LaunchMode.inAppWebView);
+          // print("platformDefault: $try2");
+
           if (try3 == false) {
-            final bool nativeAppLaunchSucceeded = await launchUrl(uri);
+            final bool nativeAppLaunchSucceeded = await launchUrl(
+              uri,
+            );
+            // print("nativeAppLaunchSucceeded: $nativeAppLaunchSucceeded");
+
             return nativeAppLaunchSucceeded;
           } else {
             return true;
@@ -32,12 +47,13 @@ class ServiceURLLauncher {
     return false;
   }
 
-  Future<void> openTwitterUser(String username, BuildContext context) async {
-    return launchStringUrl('https://twitter.com/$username');
+  Future<bool> openTwitterUser(String username, BuildContext context) async {
+    return launchUniversalLinkIos('https://twitter.com/$username');
   }
 
   Future<void> openTwitterTag(String tag, BuildContext context) async {
     String cleanTag = tag.replaceAll('#', '');
+
     cleanTag = Uri.encodeFull(cleanTag);
 
     // print(_tag);
@@ -61,10 +77,7 @@ class ServiceURLLauncher {
       final String url = 'https://wa.me/$num?text=$msg';
       final bool nativeAppLaunchSucceeded = await launchUniversalLinkIos(url);
       if (nativeAppLaunchSucceeded == false) {
-        final bool urlLaunch = await launchUrl(Uri.dataFromString(url));
-        if (urlLaunch == false) {
-          MyServices.services.snackBar.showText(text: num);
-        }
+        MyServices.services.snackBar.showText(text: num);
       }
     }
   }
