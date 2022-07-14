beedd6cd5711082bf8daa2228102fb0279bd3ff4
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-07-14 09:25:07 +0300

    Update service_loader.dart

--- lib/services/service_loader.dart
+++ lib/services/service_loader.dart
@@ -5,20 +5,29 @@ class ServiceLoader {
   factory ServiceLoader() => _s;
   const ServiceLoader._();
   //
-  static final _loadings = ChangeNotifierProvider<_LoadersNotifier>((ref) => _LoadersNotifier());
+  static final ChangeNotifierProvider<_LoadersNotifier> _loadings = ChangeNotifierProvider<_LoadersNotifier>((ref) => _LoadersNotifier());
 
   static bool isLoading(dynamic ref, dynamic name) => ref.watch(_loadings).isLoading(name);
 
-  static void setLoading(dynamic ref, dynamic name, bool status) => ref.read(_loadings.notifier).setLoading(name, status);
+  static void setLoading(dynamic ref, dynamic name, bool status, [int expireSecond = 100]) => ref.read(_loadings.notifier).setLoading(name, status, expireSecond);
 }
 
 class _LoadersNotifier extends ChangeNotifier {
   Map<dynamic, bool> state = {};
 
-  bool isLoading(dynamic name) => state[name] == true;
+  bool isLoading(dynamic name) {
+    return state[name] == true;
+  }
 
-  void setLoading(dynamic name, bool status) {
+  void setLoading(dynamic name, bool status, int expireSecond) {
     state[name] = status;
+
+    if (status) {
+      Timer(Duration(seconds: expireSecond), () {
+        state[name] = false;
+        notifyListeners();
+      });
+    }
     notifyListeners();
   }
 }
