709585afda623df92f91ba9063593f546afc1495
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-07-23 17:18:30 +0300

    Update service_dialog.dart

--- lib/services/service_dialog.dart
+++ lib/services/service_dialog.dart
@@ -1,66 +1,68 @@
 import '../my_services.dart';
 
 class ServiceDialog {
-  // const ServiceDialog._();
-  // static const ServiceDialog _s = ServiceDialog._();
-  // factory ServiceDialog() => _s;
-  //
+  const ServiceDialog();
+
   Future<T?> show<T>({String? title, List<Widget>? children, Widget? child, bool barrierDismissible = true, EdgeInsets? insetPadding, EdgeInsets? contentPadding}) {
     return showDialog<T?>(
-        context: MyServices.context,
-        barrierDismissible: barrierDismissible,
-        builder: (BuildContext c) {
-          return GestureDetector(
-            onTap: () => barrierDismissible ? pop() : null,
-            child: MyDialog(title: title, child: child, children: children, insetPadding: insetPadding, contentPadding: contentPadding),
-          );
-        });
+      context: MyServices.context,
+      barrierDismissible: barrierDismissible,
+      builder: (BuildContext c) {
+        return GestureDetector(
+          onTap: () => barrierDismissible ? pop(false) : null,
+          child: MyDialog(title: title, child: child, children: children, insetPadding: insetPadding, contentPadding: contentPadding),
+        );
+      },
+    );
   }
 
-  Future<bool?> showYesNo({required String title, required String question, bool barrierDismissible = true}) {
-    return showDialog<bool>(
-        context: MyServices.context,
-        barrierDismissible: barrierDismissible,
-        builder: (BuildContext context) {
-          return GestureDetector(
-            onTap: () => barrierDismissible ? pop(false) : null,
-            child: MyDialog(
-              title: title,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(question, style: getTextTheme(context).titleMedium),
-                      ),
-                      Row(
-                        children: [
-                          TextButton(
-                            child: Text(
-                              getMyServicesLabels(context).cancel,
-                              style: getTextTheme(context).titleMedium?.copyWith(color: Colors.green),
-                            ),
-                            onPressed: () => pop(false),
-                          ),
-                          const Spacer(),
-                          TextButton(
-                            child: Text(
-                              getMyServicesLabels(context).continue1,
-                              style: getTextTheme(context).titleMedium?.copyWith(color: Colors.red.shade800),
-                            ),
-                            onPressed: () => pop(true),
-                          ),
-                        ],
-                      ),
-                    ],
+  Future<bool?> showYesNo({
+    String? title,
+    String? questionText,
+    String? buttonText,
+    required VoidCallback onYes,
+    bool? success = false,
+    bool barrierDismissible = true,
+  }) {
+    return show(title: title, barrierDismissible: barrierDismissible, children: [
+      Builder(builder: (context) {
+        final MyServicesLocalizationsData labels = getMyServicesLabels(context);
+
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(questionText ?? labels.areYouSure, style: getTextTheme(context).titleMedium),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    child: Text(
+                      getMyServicesLabels(context).cancel,
+                      style: getTextTheme(context).titleMedium?.copyWith(color: Colors.green),
+                    ),
+                    onPressed: () => pop(false),
                   ),
-                )
-              ],
-            ),
-          );
-        });
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {
+                      onYes();
+                      pop(true);
+                    },
+                    child: Text(
+                      getMyServicesLabels(context).continue1,
+                      style: getTextTheme(context).titleMedium?.copyWith(color: Colors.red.shade800),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      })
+    ]);
   }
 }
 
@@ -81,18 +83,23 @@ class MyDialog extends StatelessWidget {
   final EdgeInsets? contentPadding;
 
   Widget dialog(BuildContext context) {
+    EdgeInsets nContentPadding = contentPadding ?? const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 16.0);
+    EdgeInsets nInsetPadding = insetPadding ?? const EdgeInsets.symmetric(horizontal: 40, vertical: 24);
+    MyText? nTitle = title != null ? MyText(title, textAlign: TextAlign.center) : null;
+
     if (children != null) {
       return SimpleDialog(
-        contentPadding: contentPadding ?? const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 16.0),
-        insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-        title: title != null ? MyText(title, textAlign: TextAlign.center) : null,
+        contentPadding: nContentPadding,
+        insetPadding: nInsetPadding,
+        title: nTitle,
         children: children,
       );
     }
+
     return AlertDialog(
-      contentPadding: contentPadding ?? const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 16.0),
-      insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-      title: title != null ? MyText(title, textAlign: TextAlign.center) : null,
+      contentPadding: nContentPadding,
+      insetPadding: nInsetPadding,
+      title: nTitle,
       content: SizedBox(
         width: double.maxFinite, //width: double.maxFinite in case the child was a ListView
         child: Unfocus(child: child),
