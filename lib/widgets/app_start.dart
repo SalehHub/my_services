e0c0a10f70e7394fb3657ead15691c208c3a1ce6
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-10-25 12:14:41 -0500

    Update app_start.dart

--- lib/widgets/app_start.dart
+++ lib/widgets/app_start.dart
@@ -106,11 +106,11 @@ class AppLauncher {
     }
 
     //start-firebaseCrashlytics
-    //
+    
     if (MyServices.appConfig.withCrashlytics && !testing) {
       MyServices.services.firebaseCrashlytics.register();
     }
-    //
+    
     //end-firebaseCrashlytics
 
     if (initGeneralState && !testing) {
